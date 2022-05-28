91d8fa1be5dd83087082237293a523cc8cc9b12f
hungvx <vuxuanhungmta@gmail.com>
2022-05-28 14:30:51 +0700

    ssss

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
-const express = require('express')
-const redis = require('redis')
+const express = require("express");
+const { createClient } = require("redis");
+const app = express();
+const session = require("express-session");
+let RedisStore = require("connect-redis")(session);
+//let redisClient = createClient({ legacyMode: true });
+let redisClient = createClient({ host: "redis-server", port: 6379 });
+redisClient.connect().catch();
 
-const app = express()
-const client = redis.createClient({
-    host: 'redis-server',
-    port: 6379
-})
+app.use(
+    session({
+        store: new RedisStore({ client: redisClient }),
+        secret: "config.SECRET",
+        resave: false,
+        saveUninitialized: true,
+        cookie: { maxAge: 24 * 60 * 60 * 1000, httpOnly: false, secure: false }, // mini second hour * 60 * 60 * 1000
+    })
+);
 
 //Set initial visits
-client.set('visits', 0);
+redisClient.set("visits", 0);
 
 //defining the root endpoint
-app.get('/', (req, res) => {
-    client.get('visits', (err, visits) => {
-        res.send('Number of visits is: ' + visits)
-        client.set('visits', parseInt(visits) + 1)
-    })
-})
+app.get("/", (req, res) => {
+    let oldCount = req.session.count || 0;
+    redisClient.get("visits", (err, visits) => {
+        req.session.count = parseInt(oldCount) + 1;
+        res.send("Number of visits is: " + oldCount);
+        redisClient.set("visits", parseInt(oldCount) + 1);
+        //console.log(req.session.count);
+    });
+});
+
+//defining the root endpoint
+app.get("/withoutredis", (req, res) => {
+    redisClient.get("visits", (err, visits) => {
+        res.send("Number of visits is: " + visits);
+        redisClient.set("visits", parseInt(visits) + 1);
+    });
+});
 
 //specifying the listening port
-app.listen(8081, ()=>{
-    console.log('Listening on port 8081')
-})
+app.listen(8081, () => {
+    console.log("Listening on port 8081");
+});
